76e719b0bdd26d073dd28fe8f3dbb3084fdaf566
JonathanGithumbi <jonathan.m.githumbi@gmail.com>
2022-05-13 18:04:31 +0300

    added arrears to financial account

--- financial_account/models.py
+++ financial_account/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from student.models import Student
 
 class FinancialAccount(models.Model):
-    """a financial account constitutes of financial statements"""
-
+    """a financial account constitutes of financial transactions"""
     def __str__(self):
         return self.student.__str__()
-        
+    
+    TRANSACTION_TYPE_CHOICES = (
+        ('credit','Credit'),
+        ('debit','Debit')
+    )
+    transaction_type = models.CharField(max_length=7,choices=TRANSACTION_TYPE_CHOICES,default='credit')
     date_of_payment = models.DateTimeField(auto_now_add=True)
     amount = models.DecimalField(max_digits=8,decimal_places=2)
+    arrears = models.DecimalField(max_digits=8,decimal_places=2)
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
+
+
